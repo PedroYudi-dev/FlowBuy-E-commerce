b0e954139a404a275d645908a552c7fa25ffd6ba
Pedro Yudi <pedro.yudi005@gmail.com>
2025-10-04 21:34:45 -0300

    Foi adicionado novos icones de pagamento e mudamos a cor do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,15 @@ import "./Footer.css"
 import { Link } from "react-router-dom"
 
 // Icones
-import { Phone, LockKeyhole, Mail, MapPin } from "lucide-react";
+import { Phone, LockKeyhole, Mail, MapPin, FacebookIcon, Instagram, LinkedinIcon, Twitter } from "lucide-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCcVisa,
-  faCcMastercard,
-  faPix,
-  faPaypal,
-  faFacebook,
-  faInstagram,
-  faTiktok,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import IconMasterCard from "../../elements/Cards/IconMasterCard";
+import VisaCard from "../../elements/Cards/IconVisaCard";
+import PaypalCard from "../../elements/Cards/IconPayPall";
+import PixLogo from "../../elements/Cards/IconPix";
 
 // state
 
@@ -22,7 +19,16 @@ import {
 export default function Footer(){
 
   const locationMarket = "https://www.google.com/maps/place/R.+Amazonas,+734+-+Banzato,+Mar%C3%ADlia+-+SP,+17515-160/data=!4m2!3m1!1s0x94bfd733850c2799:0x4e5c0c6228a2422d?sa=X&ved=1t:242&ictx=111&cshid=1756303824088467"
-  
+
+
+  const IconVisa = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 60" width="100" height="60">
+  <text x="0" y="45" font-family="Arial, sans-serif" font-size="50" fill="white">VISA</text>
+</svg>
+  const IconMastercard = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 60" width="100" height="60">
+  <circle cx="35" cy="30" r="20" fill="white" opacity="0.8"/>
+  <circle cx="65" cy="30" r="20" fill="white" opacity="0.8"/>
+</svg>
+
     return (
       <>
         <div id="container-footer">
@@ -52,15 +58,15 @@ export default function Footer(){
               <div className="contact-inform">
                 <h2>Contato</h2>
                 <div className="phone">
-                  <Phone color="#0b14c8" />
+                  <Phone color="#bebebe" />
                   <p>(14) 3458-7478 - Atendimento das 8h até 18h </p>
                 </div>
                 <div className="email">
-                  <Mail color="#0b14c8" />
+                  <Mail color="#bebebe" />
                   <p>Flow@FlowBuy.com</p>
                 </div>
                 <div className="location">
-                  <MapPin color="#0b14c8" />
+                  <MapPin color="#bebebe" />
                   <Link to={locationMarket} target="_blank">
                     <p>Rua Amazonas 734 Marília/SP</p>
                   </Link>
@@ -72,29 +78,21 @@ export default function Footer(){
                 <h2>Rede Socias</h2>
                 <div className="network">
                   <p>
-                    <FontAwesomeIcon
-                      className="network-icons"
-                      icon={faFacebook}
-                    />
+                    <FacebookIcon className="network-icons" color="#bebebe" />
                     @FlowBuyStore
                   </p>
                   <p>
-                    <FontAwesomeIcon
-                      className="network-icons"
-                      icon={faInstagram}
-                    />
+                    <Instagram className="network-icons" color="#bebebe" />
                     @FlowBuyStore
                   </p>
                   <p>
-                    <FontAwesomeIcon
-                      className="network-icons"
-                      icon={faTiktok}
-                    />
+                    <LinkedinIcon className="network-icons" color="#bebebe" />
                     @FlowBuyStore
                   </p>
                   <p>
-                    <FontAwesomeIcon
+                    <Twitter
                       className="network-icons"
+                      color="#bebebe"
                       icon={faTwitter}
                     />
                     @FlowBuyStore
@@ -106,16 +104,16 @@ export default function Footer(){
           <div id="orter-onformation">
             <div className="security">
               <p>
-                <LockKeyhole className="lock" color="#0b14c8" />
+                <LockKeyhole className="lock" color="#bebebe" />
                 Site Seguro
               </p>
               <p>Seus Dados estão Protegidos conforme a LGPD</p>
             </div>
             <div className="cards">
-              <FontAwesomeIcon className="cards-pay" icon={faCcVisa} />
-              <FontAwesomeIcon className="cards-pay" icon={faCcMastercard} />
-              <FontAwesomeIcon className="cards-pay" icon={faPix} />
-              <FontAwesomeIcon className="cards-pay" icon={faPaypal} />
+              <VisaCard />
+              <IconMasterCard />
+              <PixLogo />
+              <PaypalCard />
             </div>
           </div>
         </div>
